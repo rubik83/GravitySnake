4458e7d947d58b9544c1159845c55fe39726137b
François SOLANET <francois.solanet@effidence.com>
2022-09-19 11:04:16 +0200

    Black background

--- GraphicsDrawable.cs
+++ GraphicsDrawable.cs
@@ -2,23 +2,12 @@ namespace GravitySnake;
 
 public partial class GraphicsDrawable : IDrawable
 {
-    //public AccelerometerData Acc;
     internal Snake.Program Prog = new Snake.Program(new System.Drawing.Size { Width = 250, Height = 250 });
 
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
+        canvas.FillColor = Colors.Black;
+        canvas.FillRectangle(dirtyRect);
         Prog.Draw(canvas, dirtyRect);
-        /*
-        canvas.StrokeColor = Colors.Red;
-        canvas.StrokeSize = 6;
-        canvas.DrawLine(
-            dirtyRect.Center.X,
-            dirtyRect.Center.Y,
-            (float)(dirtyRect.Center.X - (Acc.Acceleration.X * dirtyRect.Width / 2.0)),
-            (float)(dirtyRect.Center.Y + (Acc.Acceleration.Y * dirtyRect.Height / 2.0))
-        );
-        canvas.DrawRoundedRectangle(dirtyRect, 10, 10);
-        */
-
     }
 }
